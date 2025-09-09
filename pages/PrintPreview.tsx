735825ac198cb95c51a42953e4285c4f484d8801
Veladrywash <veladrywash@gmail.com>
2025-09-09 06:43:37 +0530

    Update PrintPreview.tsx

--- pages/PrintPreview.tsx
+++ pages/PrintPreview.tsx
@@ -60,7 +60,7 @@ export default function PrintPreview() {
 
         <hr className="receipt-hr" />
 
-        <div className="receipt-small">Order Date: {new Date(order.createdAt).toLocaleString()}</div>
+  <div className="receipt-small">Order Date: {new Date(order.createdAt).toLocaleDateString()}</div>
         {order.deliveryDate && (
           <div className="receipt-small">Delivery Date: {new Date(order.deliveryDate).toLocaleDateString()}</div>
         )}
